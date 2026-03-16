4f8ab7640d7c8644eaf88441927db26fefb131b9
xcxz <xcxz@MacBook.local>
2026-03-16 22:29:07 +0100

    file

--- worker.py
+++ worker.py
@@ -15,7 +15,7 @@ from telethon.tl.functions.channels import JoinChannelRequest
 
 REACTIONS = ["😘", "❤️", "😍", "🥰", "🔥", "😱", "🤩", "💯"]
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 CHAT_LINKS_PATH = os.path.join(BASE_DIR, "data", "chat_links.txt")
 CHANNEL_LINKS_PATH = os.path.join(BASE_DIR, "data", "channel_links.txt")
 
